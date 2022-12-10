3307ca7d3c6b1ef95fe3692c8e31e2cd5bcec423
PsykoDev <45910905+PsykoDev@users.noreply.github.com>
2022-12-10 04:52:39 +0100

    Add files via upload

---
+++ ChiffrementAES.cs
@@ -0,0 +1,57 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace ouaiouai
+{
+    internal class Program
+    {        
+        static void Main(string[] args)
+        {
+            string key = "MySuperKeyTopSecretToSecurMyData"; // 32 char minimal for AES key 
+            string plainText = "This is the plain text to be encrypted.";
+
+            string encrypted = Encrypt(plainText, key);
+            string decrypted = Decrypt(encrypted, key);
+
+            Console.WriteLine("Plain text: {0}", plainText);
+            Console.WriteLine("Encrypted: {0}", encrypted);
+            Console.WriteLine("Decrypted: {0}", decrypted);
+        }
+
+        static string Encrypt(string data, string key)
+        {
+            byte[] dataBytes = Encoding.UTF8.GetBytes(data);
+            byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+
+            using (Aes encryptor = Aes.Create())
+            {
+                encryptor.Mode = CipherMode.ECB;
+                encryptor.Key = keyBytes;
+
+                ICryptoTransform encryptorTransform = encryptor.CreateEncryptor();
+
+                byte[] encryptedData = encryptorTransform.TransformFinalBlock(dataBytes, 0, dataBytes.Length);
+
+                return Convert.ToBase64String(encryptedData);
+            }
+        }
+
+        static string Decrypt(string data, string key)
+        {
+            byte[] dataBytes = Convert.FromBase64String(data);
+            byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+
+            using (Aes encryptor = Aes.Create())
+            {
+                encryptor.Mode = CipherMode.ECB;
+                encryptor.Key = keyBytes;
+
+                ICryptoTransform decryptorTransform = encryptor.CreateDecryptor();
+
+                byte[] decryptedData = decryptorTransform.TransformFinalBlock(dataBytes, 0, dataBytes.Length);
+
+                return Encoding.UTF8.GetString(decryptedData);
+            }
+        }
+    }
+}
---
+++ MergeSort.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ouaiouai
+{
+    public static class MergeSort
+    {
+        public static void Sort<T>(T[] array, int start, int end) where T : IComparable<T>
+        {
+            if (start < end)
+            {
+                int middle = (start + end) / 2;
+                Sort(array, start, middle);
+                Sort(array, middle + 1, end);
+                Merge(array, start, middle, end);
+            }
+        }
+
+        private static void Merge<T>(T[] array, int start, int middle, int end) where T : IComparable<T>
+        {
+            T[] left = new T[middle - start + 1];
+            T[] right = new T[end - middle];
+
+            Array.Copy(array, start, left, 0, middle - start + 1);
+            Array.Copy(array, middle + 1, right, 0, end - middle);
+
+            int i = 0;
+            int j = 0;
+            int k = start;
+
+            while (i < left.Length && j < right.Length)
+            {
+                if (left[i].CompareTo(right[j]) <= 0)
+                {
+                    array[k] = left[i];
+                    i++;
+                }
+                else
+                {
+                    array[k] = right[j];
+                    j++;
+                }
+                k++;
+            }
+
+            while (i < left.Length)
+            {
+                array[k] = left[i];
+                i++;
+                k++;
+            }
+
+            while (j < right.Length)
+            {
+                array[k] = right[j];
+                j++;
+                k++;
+            }
+        }
+    }
+
+}
